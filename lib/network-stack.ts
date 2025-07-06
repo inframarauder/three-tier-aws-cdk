be53d1aca330975ce834f7f4a9e4cfbd48a573b9
Subhasis Das <subhasisdas125@gmail.com>
2025-07-06 18:23:26 +0530

    added networking stack

---
+++ lib/network-stack.ts
@@ -0,0 +1,61 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Vpc, SubnetConfiguration, SubnetType } from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+import * as Types from '../types';
+
+export interface NetworkStackProps extends StackProps {
+    environment: Types.ENVIRONMENT;
+    vpcNamePrefix: string;
+    vpcCidrIPv4: string;
+    publicSubnetCidrMask: number;
+    privateSubnetCidrMask: number;
+    databaseSubnetCidrMask: number;
+    maxAZs: number;
+    numNatGateways: number;
+}
+
+export interface NetworkStackOutputs {
+    vpc: Vpc;
+}
+
+export class NetworkStack extends Stack {
+    private readonly vpc: Vpc;
+
+    constructor(scope: Construct, id: string, props: NetworkStackProps) {
+        super(scope, id, props);
+
+        // define subnet configurations
+        const subnetConfiguration: SubnetConfiguration[] = [
+            {
+                name: "Public",
+                subnetType: SubnetType.PUBLIC,
+                cidrMask: props.publicSubnetCidrMask
+            },
+            {
+                name: "Private-App",
+                subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+                cidrMask: props.privateSubnetCidrMask
+            },
+            {
+                name: "Private-DB",
+                subnetType: SubnetType.PRIVATE_ISOLATED,
+                cidrMask: props.databaseSubnetCidrMask
+            }
+        ];
+
+        // create VPC with subnet configs
+        this.vpc = new Vpc(this, 'ThreeTierVpc', {
+            vpcName: `${props.vpcNamePrefix}-${props.environment}`,
+            cidr: props.vpcCidrIPv4,
+            maxAzs: props.maxAZs,
+            natGateways: props.numNatGateways,
+            subnetConfiguration
+        });
+    }
+
+    getOutputs(): NetworkStackOutputs {
+        return {
+            vpc: this.vpc
+        };
+    };
+}
