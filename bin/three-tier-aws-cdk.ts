19f8e956967809285c5f52b7fc2c439860e5285b
Subhasis Das <subhasisdas125@gmail.com>
2025-07-06 19:00:22 +0530

    added default tags

--- bin/three-tier-aws-cdk.ts
+++ bin/three-tier-aws-cdk.ts
@@ -3,7 +3,12 @@ import * as cdk from 'aws-cdk-lib';
 import { NetworkStack } from '../lib/network-stack';
 import * as DevConfigs from '../environments/dev';
 
+// create CDK app
 const app = new cdk.App();
+
+// define and apply default tags
+cdk.Tags.of(app).add('aws-cdk', 'true');
+cdk.Tags.of(app).add('auto-destroy', 'true'); // needed for lambda to auto destroy resources, in case i forget!!
 
 // create NetworkStack
 new NetworkStack(app, 'NetworkStack', DevConfigs.NetworkStackProps);
